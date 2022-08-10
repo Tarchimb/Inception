e3ffdc42cf65aec08d401e052e5eac9cb1d744b2
Archimbaud <tharchimb@gmail.com>
2022-08-10 10:32:35 +0200

    Add volumes to nginx

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# if [ ! -d "/certs" ]
-# then
-# 	mkdir /certs; cp /etc/certs/fullchain.pem /certs/certificat.crt
-# fi
+if [ ! -d "/certs" ]
+then
+	mkdir /certs; cp /etc/certs/fullchain.pem /certs/certificat.crt
+fi
 nginx -g "daemon off;"
 # openssl x509 -outform pem -in /etc/certs/fullchain.pem -out certs/certificat
