e477bd17f2b5740c9e57871f5643184350aecf27
Archimbaud <tharchimb@gmail.com>
2022-08-10 11:19:00 +0200

    save

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -d "/certs" ]
-then
-	mkdir /certs; cp /etc/certs/fullchain.pem /certs/certificat.crt
-fi
+mkdir /certs && cp /etc/certs/fullchain.pem /certs/certificat.crt
 nginx -g "daemon off;"
 # openssl x509 -outform pem -in /etc/certs/fullchain.pem -out certs/certificat
